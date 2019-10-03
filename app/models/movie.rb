08b6c90704d1f3831dbb83a6c671e1f8de58f0bc
Osama Aftab Toor <osama.aftab.1811@gmail.com>
2019-10-03 20:24:48 +0000

    Part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+    def self.get_all_ratings
+        Movie.uniq.pluck(:rating)
+    end
+    def self.get_sorted(sort_key)
+        Movie.order(sort_key)
+    end
+    def self.with_ratings(ratings)
+        Movie.where(rating: ratings)
+    end
 end
