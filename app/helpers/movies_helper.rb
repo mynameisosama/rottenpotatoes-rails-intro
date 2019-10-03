55e3d7d89f950a7bc9c295bc132e0a4c1ebac929
Osama Aftab Toor <osama.aftab.1811@gmail.com>
2019-10-03 22:53:58 +0000

    Part3 complete

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -8,11 +8,38 @@ module MoviesHelper
    (params[:sort].to_s == field) ? 'hilite' : nil
   end
   
-  def helper_rating(rating, default)
-    rating == nil ? default: rating.keys
+  def sanitize_rating(default)
+    (params.include?(:ratings)) ? params[:ratings].keys : default
   end
   
   def which_checked(rating)
-    params[:ratings] ? params[:ratings].include?(rating) : true
+     (params.include?(:ratings)) ? params[:ratings].include?(rating) : true
+  end
+  
+  def valid_ratings()
+     params.include?(:ratings)
+  end
+  
+  def valid_sort_key()
+     params.include?(:sort)
+  end
+  
+  def update_session()
+    session[:ratings] = params[:ratings] ? params[:ratings] : session[:ratings]
+    session[:sort] = params[:sort] ? params[:sort] : session[:sort]
+  end
+  
+  def invalid_params()
+    invalid_rating = false
+    invalid_sort_key= false
+    if(!valid_ratings() and session.include?(:ratings))
+      params[:ratings] = session[:ratings]
+      invalid_rating = true
+    end
+    if(!valid_sort_key() and session.include?(:sort))
+      params[:sort] = session[:sort]
+      invalid_sort_key = true
+    end
+    return (invalid_rating or invalid_sort_key)
   end
 end
